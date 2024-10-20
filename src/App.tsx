1bc41d526a5acbea75fce1d7697876ae92e8e1fb
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-10-20 19:28:01 +0300

    copied previous task src

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { getTodos, USER_ID } from './api/todos';
+import { Todo } from './types/Todo';
+import { Filters } from './types/Filters';
+import { Header } from './blocks/Header';
+import { Footer } from './blocks/Footer';
+import { TodoList } from './components/TodoList';
+import { ErrorNotification } from './components/ErrorNotification';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filteredTodos, setFilteredTodos] = useState<Todo[]>([]);
+  const [currentError, setCurrentError] = useState<string | null>(null);
+  const [currentFilter, setCurrentFilter] = useState<Filters>(Filters.all);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(error => {
+        setCurrentError(error.message);
+        setTimeout(() => setCurrentError(null), 3000);
+      });
+  }, []);
+
+  useEffect(() => {
+    setFilteredTodos(todos);
+  }, [todos]);
+
+  const onFilterChange = (filter: Filters) => {
+    if (currentFilter === filter) {
+      return;
+    }
+
+    setCurrentFilter(filter);
+
+    const { all, active, completed } = Filters;
+
+    switch (filter) {
+      case all:
+        setFilteredTodos(todos);
+        break;
+      case active:
+        setFilteredTodos(todos.filter(todo => !todo.completed));
+        break;
+      case completed:
+        setFilteredTodos(todos.filter(todo => todo.completed));
+        break;
+    }
+  };
+
+  const isAllCompleted = useMemo(() => {
+    return filteredTodos.every(todo => todo.completed);
+  }, [filteredTodos]);
+
+  const uncompletedCount = useMemo(() => {
+    return todos.reduce((acc, todo) => (todo.completed ? acc : acc + 1), 0);
+  }, [todos]);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header filteredTodos={filteredTodos} isAllCompleted={isAllCompleted} />
+
+        <TodoList filteredTodos={filteredTodos} />
+
+        {/* Hide the footer if there are no todos */}
+        {todos.length > 0 && (
+          <Footer
+            uncompletedCount={uncompletedCount}
+            currentFilter={currentFilter}
+            onFilterChange={onFilterChange}
+          />
+        )}
+      </div>
+
+      {/* DON'T use conditional rendering to hide the notification */}
+      {/* Add the 'hidden' class to hide the message smoothly */}
+      <ErrorNotification currentError={currentError} />
+    </div>
   );
 };
